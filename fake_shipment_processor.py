452324f19d13037c689f75920a91e93c97eca71d
David Gethers <dgethers.jr@gmail.com>
2024-07-02 16:02:53 -0400

    Add ability to filter shipments

--- fake_shipment_processor.py
+++ fake_shipment_processor.py
@@ -11,7 +11,7 @@ class ShipmentProcessor(Protocol):
 class FakeShipmentProcessor(ShipmentProcessor):
 
     def __init__(self, filename: str):
-        self.shipments = self._load_shipments_from_file(filename)
+        self.all_shipments = self._load_shipments_from_file(filename)
 
     @staticmethod
     def _load_shipments_from_file(filename):
@@ -20,4 +20,9 @@ class FakeShipmentProcessor(ShipmentProcessor):
             return data['shipments']
 
     def get_shipments(self, start_date: datetime = None, end_date: datetime = None):
-        return self.shipments
+        if start_date and end_date:
+            return [shipment for shipment in self.all_shipments if
+                    datetime.strptime(shipment['create_date'], "%Y-%m-%d") <= end_date and datetime.strptime(
+                        shipment['create_date'], "%Y-%m-%d") >= start_date]
+
+        return self.all_shipments
